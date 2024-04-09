db7f19c935a8d11ce3924ce42965a5d85e6b7dba
Reza Abdolmaleki <abdolmaleki.reza@outlook.com>
2024-04-09 11:56:54 +0330

    Add Common class named AuditableEntity.CS this class is Base for other entity

---
+++ Domain/Common/AudtibleEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GTMSystem.Domain.Common;
+
+public class AudtibleEntity
+{
+    public string? CreatedBy { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string? LastModifiedBy { get; set; }
+    public DateTime? LastModifiedDate { get; set; }
+}
